54050f1c4dfb0be74650546d8b045bb5edc70753
William Koller <wkoller25@gmail.com>
2020-07-24 20:36:55 -0300

    add for of

---
+++ for-of.js
@@ -0,0 +1,18 @@
+let myArray = [2,4,6,8]
+
+/**
+ * for of percorre pelos valores de objetos que sejam iteratvos,
+ * como por exemplo um Array, map, set, etc
+ */
+for (let value of myArray) {
+  console.log(value)
+}
+
+let veiculo = 'Bicicleta'
+/**
+ * conseguimos ter cada letra da string separadas, 
+ * usando o for of.
+ */
+for (let letter of veiculo) {
+  console.log(letter)
+}
